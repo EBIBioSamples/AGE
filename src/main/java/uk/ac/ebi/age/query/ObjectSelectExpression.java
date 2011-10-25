cae36f596dbf446f398caef03fc32097b20f14ee
Adam Faulconbridge <afualconbridge@googlemail.com>
2011-10-25 17:03:19 +0100

    Moved to maven structure

--- src/main/java/uk/ac/ebi/age/query/ObjectSelectExpression.java
+++ src/main/java/uk/ac/ebi/age/query/ObjectSelectExpression.java
@@ -8,26 +8,24 @@ public class ObjectSelectExpression implements QueryExpression
  public void setAgeClassName(String string)
  {
   // TODO Auto-generated method stub
-  throw new dev.NotImplementedYetException();
  }
 
  public void setCondition(QueryExpression selExpr)
  {
   // TODO Auto-generated method stub
-  throw new dev.NotImplementedYetException();
  }
 
  public boolean test(AgeObject obj)
  {
   // TODO Auto-generated method stub
-  throw new dev.NotImplementedYetException();
+  return false;
  }
 
  @Override
  public boolean isTestingRelations()
  {
   // TODO Auto-generated method stub
-  throw new dev.NotImplementedYetException();
+  return false;
  }
 
 }
